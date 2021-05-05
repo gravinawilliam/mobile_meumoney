acca00863468ca8c126004bd16b6aefdaf61bfe9
William Gravina <gravinawilliam@viwcode.com.br>
2021-05-04 21:01:03 -0300

    :wrench: config intl

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,12 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+
+import 'shared/constants/constants.dart';
 
 class AppWidget extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Slidy',
-      theme: ThemeData(primarySwatch: Colors.blue),
-    ).modular();
-  }
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'MeuMoney',
+        debugShowCheckedModeBanner: false,
+        theme: ThemesConst.dark,
+        supportedLocales: [
+          Locale('en', 'US'),
+          Locale('pt', 'BR'),
+        ],
+        localizationsDelegates: [
+          GlobalMaterialLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+        ],
+        localeResolutionCallback: (locale, supportedLocales) {
+          for (var supportedLocale in supportedLocales) {
+            if (supportedLocale.languageCode == locale?.languageCode &&
+                supportedLocale.countryCode == locale?.countryCode) {
+              return supportedLocale;
+            }
+          }
+          return supportedLocales.first;
+        },
+      ).modular();
 }
