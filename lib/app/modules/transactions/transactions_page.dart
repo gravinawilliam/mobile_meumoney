06853cc8800f92791fe896a3bfe908061c0d28ea
vmodesto <vmodesto2002@gmail.com>
2021-05-28 12:38:44 -0300

    apaguei a scrollController

--- lib/app/modules/transactions/transactions_page.dart
+++ lib/app/modules/transactions/transactions_page.dart
@@ -3,14 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
-import 'package:loading_indicator/loading_indicator.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 import '../../shared/constants/constants.dart';
 import '../../shared/models/models.dart';
 import '../../shared/widgets/widgets_globais.dart';
 import 'transactions_store.dart';
-import 'widgets/all_widgets/transaction_item.dart';
+import 'widgets/transaction_widgets.dart';
 
 class TransactionsPage extends StatefulWidget {
   @override
@@ -19,28 +18,6 @@ class TransactionsPage extends StatefulWidget {
 
 class TransactionsPageState
     extends ModularState<TransactionsPage, TransactionsStore> {
-  ScrollController _scrollController = ScrollController();
-  int _currentMax = 10;
-  List list = [];
-  @override
-  void initState() {
-    list = List.generate(10, (index) => "item $index");
-    _scrollController.addListener(() {
-      if (_scrollController.position.pixels ==
-          _scrollController.position.maxScrollExtent) {
-        _getMoreList();
-      }
-    });
-    super.initState();
-  }
-
-  _getMoreList() {
-    for (var i = _currentMax; i < _currentMax + 10; i++) {
-      list.add("item $i");
-    }
-    _currentMax += 10;
-  }
-
   @override
   Widget build(BuildContext context) {
     SizeConst().init(context);
@@ -89,70 +66,58 @@ class TransactionsPageState
               ),
             ),
             ListView.builder(
-              shrinkWrap: true,
+              scrollDirection: Axis.vertical,
               physics: NeverScrollableScrollPhysics(),
-              controller: _scrollController,
-              itemBuilder: (context, index) {
-                if (index == list.length) {
-                  return LoadingIndicator(indicatorType: Indicator.ballBeat);
-                }
-                return Text(index.toString());
-              },
-            )
-
-            // ListView.builder(
-            //   scrollDirection: Axis.vertical,
-            //   physics: NeverScrollableScrollPhysics(),
-            //   shrinkWrap: true,
-            //   itemCount: 10,
-            //   itemBuilder: (context, index) => Slidable(
-            //     actionPane: SlidableDrawerActionPane(),
-            //     actionExtentRatio: 0.25,
-            //     child: TransactionItem(
-            //       length: 10 - 1,
-            //       index: index,
-            //       transaction: TransactionModel(
-            //         date: "2021-12-22",
-            //         fromBankAccountId: "id",
-            //         id: "id",
-            //         note: "a",
-            //         symbolCoin: "BRl",
-            //         title: "Com",
-            //         transactionType: "gain",
-            //         userId: "id",
-            //         value: 35000.50,
-            //       ),
-            //     ),
-            //     actions: <Widget>[
-            //       IconSlideAction(
-            //         caption: 'Archive',
-            //         color: Colors.blue,
-            //         icon: Icons.archive,
-            //         onTap: () => {},
-            //       ),
-            //       IconSlideAction(
-            //         caption: 'Share',
-            //         color: Colors.indigo,
-            //         icon: Icons.share,
-            //         onTap: () => {},
-            //       ),
-            //     ],
-            //     secondaryActions: <Widget>[
-            //       IconSlideAction(
-            //         caption: 'More',
-            //         color: Colors.black45,
-            //         icon: Icons.more_horiz,
-            //         onTap: () => {},
-            //       ),
-            //       IconSlideAction(
-            //         caption: 'Delete',
-            //         color: Colors.red,
-            //         icon: Icons.delete,
-            //         onTap: () => {},
-            //       ),
-            //     ],
-            //   ),
-            // ),
+              shrinkWrap: true,
+              itemCount: 10,
+              itemBuilder: (context, index) => Slidable(
+                actionPane: SlidableDrawerActionPane(),
+                actionExtentRatio: 0.25,
+                child: TransactionItem(
+                  length: 10 - 1,
+                  index: index,
+                  transaction: TransactionModel(
+                    date: "2021-12-22",
+                    fromBankAccountId: "id",
+                    id: "id",
+                    note: "a",
+                    symbolCoin: "BRl",
+                    title: "Com",
+                    transactionType: "gain",
+                    userId: "id",
+                    value: 35000.50,
+                  ),
+                ),
+                actions: <Widget>[
+                  IconSlideAction(
+                    caption: 'Archive',
+                    color: Colors.blue,
+                    icon: Icons.archive,
+                    onTap: () => {},
+                  ),
+                  IconSlideAction(
+                    caption: 'Share',
+                    color: Colors.indigo,
+                    icon: Icons.share,
+                    onTap: () => {},
+                  ),
+                ],
+                secondaryActions: <Widget>[
+                  IconSlideAction(
+                    caption: 'More',
+                    color: Colors.black45,
+                    icon: Icons.more_horiz,
+                    onTap: () => {},
+                  ),
+                  IconSlideAction(
+                    caption: 'Delete',
+                    color: Colors.red,
+                    icon: Icons.delete,
+                    onTap: () => {},
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
